79613f8864a554e5cd66f2bb0c5a59a38ce93b90
sliitguy <it23259416@my.sliit.lk>
2025-09-02 16:43:29 +0530

    Deployment changes

--- networksecurity/cloud/s3_syncer.py
+++ networksecurity/cloud/s3_syncer.py
@@ -3,10 +3,12 @@ import os
 
 
 class S3Sync:
+    # Sending the details from folder to s3
     def sync_folder_to_s3(self,folder,aws_bucket_url):
         command = f"aws s3 sync {folder} {aws_bucket_url} "
         os.system(command)
 
+    # Getting details back from s3 to local folder
     def sync_folder_from_s3(self,folder,aws_bucket_url):
         command = f"aws s3 sync  {aws_bucket_url} {folder} "
         os.system(command)
